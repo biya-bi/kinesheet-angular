59c819dfbfe1324148ea2be25aae66928570be55
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-24 11:22:54 -0500

    Add logged out component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { authenticationGuard } from './authentication.guard';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ObjectiveListComponent } from './objective-list/objective-list.component';
+import { LoggedOutComponent } from './views/logged-out/logged-out.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LoggedOutComponent },
     { path: 'objectives', component: ObjectiveListComponent, canActivate: [authenticationGuard] },
     { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/views/logged-out/logged-out.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-out',
+  templateUrl: './logged-out.component.html',
+  styleUrl: './logged-out.component.css'
+})
+export class LoggedOutComponent {
+
+}
