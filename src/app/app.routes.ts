c25e01827a2677951d1c7d7fd43a1cb4df354a61
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-23 22:31:42 -0500

    Add page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { authenticationGuard } from './authentication.guard';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [authenticationGuard] },
     { path: 'home', component: HomeComponent, canActivate: [authenticationGuard] },
     { path: 'login', component: LoginComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
