de26dbc56056aeaaeb537b8c0f64080172e614ae
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-24 19:16:22 -0500

    Remove user service

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { AuthenticationManager } from '../authentication-manager';
-import { UserService } from '../services/user.service';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable, filter, map } from 'rxjs';
+import { AuthenticationManager } from '../authentication-manager';
 
 @Component({
   selector: 'app-banner',
@@ -11,13 +10,11 @@ import { Observable, filter, map } from 'rxjs';
 })
 export class BannerComponent {
 
-  readonly user$ = this.userService.user$;
   readonly userProfile$ = this.authenticationManager.userProfile$;
   
   url$: Observable<string>;
 
   constructor(
-    private readonly userService: UserService,
     private readonly authenticationManager: AuthenticationManager,
     private readonly router: Router) {
     this.url$ = this.router.events.pipe(filter(e => e instanceof NavigationEnd), map(e => (e as NavigationEnd).url));
