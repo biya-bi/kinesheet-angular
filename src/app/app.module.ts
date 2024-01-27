cc2bfeb2bfeab1fdfd7d4e9e13eab8c0a82c05be
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-27 12:00:11 -0500

    Organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, RouterOutlet } from "@angular/router";
 import { OAuthModule } from "angular-oauth2-oidc";
-import { AppComponent } from "./app.component";
-import { routes } from "./app.routes";
 import { BannerComponent } from "../views/banner/banner.component";
 import { HomeComponent } from "../views/home/home.component";
 import { LoginComponent } from "../views/login/login.component";
-import { ObjectiveListComponent } from "../views/objectives/objective-list/objective-list.component";
 import { ObjectiveContainer } from "../views/objectives/objective-container/objective.container";
+import { ObjectiveListComponent } from "../views/objectives/objective-list/objective-list.component";
+import { AppComponent } from "./app.component";
+import { routes } from "./app.routes";
 
 @NgModule({
     imports: [
