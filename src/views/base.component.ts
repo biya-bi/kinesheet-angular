265e5403af693c54dee2dbc073c74198fb658afc
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-28 11:47:22 -0500

    Improve the base component

--- src/views/base.component.ts
+++ src/views/base.component.ts
@@ -3,12 +3,13 @@ import { Subject } from "rxjs";
 
 @Component({ template: '' })
 export abstract class BaseComponent implements OnDestroy {
-    private readonly destroySubject = new Subject();
+    private readonly destroySubject = new Subject<void>();
 
     readonly destroy$ = this.destroySubject.asObservable();
 
     ngOnDestroy(): void {
-        this.destroySubject.next(undefined);
+        this.destroySubject.next();
+        this.destroySubject.complete();
     }
 
 }
